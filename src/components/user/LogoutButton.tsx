c38fa5f930fa31943c5b0b09a1772e99289dda04
hyeon <hshyeon86@gmail.com>
2025-01-31 17:11:19 +0900

    [feat] register and logout

---
+++ src/components/user/LogoutButton.tsx
@@ -0,0 +1,18 @@
+import {Button} from "react-bootstrap";
+import {useNavigate} from "react-router";
+import {axiosInstance} from "../../global/axiosInstance.ts";
+
+function LogoutButton() {
+    const navigate = useNavigate();
+    const onLogout = () =>{
+        localStorage.clear()
+        axiosInstance.get('/user/logout').then(()=>{
+            navigate('/')
+        })
+    }
+    return (
+        <Button variant="secondary" onClick={onLogout}>로그아웃</Button>
+    );
+}
+
+export default LogoutButton;
